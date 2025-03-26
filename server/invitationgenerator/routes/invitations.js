1d88fd4b419a4e842d0d6bad5d7466531a958330
Bogdanpayk1993 <body.payk.1993@gmail.com>
2025-03-26 10:32:27 +0200

    Function imposition text started doing
    Function imposition text started doing.

--- server/invitationgenerator/routes/invitations.js
+++ server/invitationgenerator/routes/invitations.js
@@ -3,13 +3,30 @@ var router = express.Router();
 var BetterSqlite3 = require('better-sqlite3');
 var db = new BetterSqlite3('invitation.db');
 
+var fsPromises = require('fs/promises');
+var readFile = fsPromises.readFile;
+var writeFile = fsPromises.writeFile;
+
+var appRootPath = require('app-root-path');
+var path = appRootPath.path;
+
+var sharp = require('sharp');  
+
 router.post('/getType', function (req, res) {
     const result = db.prepare(`SELECT * FROM invitations WHERE type='${req['body']['type']}'`).all()
     res.send(result)
 })
 
-router.post('/getInvitation', function (req, res) {
-    console.log(req['body']['invitation_details'])
+router.post('/getInvitation', async function (req, res) {
+    const file = await readFile(`${path}\\public\\images\\backgrounds\\${req['body']['invitation_details']['background_image']}`)
+    const img = sharp(file)
+    const textSVG = Buffer.from(`<svg width="400" height="400">
+                        <text x="50%" y="200" font-size="40" text-anchor="middle">
+                            ${req['body']['invitation_details']['greeting']}
+                        </text>
+                    </svg>`)
+    const result = await img.composite([{ input: textSVG }]).toBuffer()
+    await writeFile(`${path}\\public\\images\\invitations\\res.png`, result)
 })
 
 module.exports = router;
