e93fdbbde161dc4507c654369a4df5391b27e504
Bogdanpayk1993 <body.payk.1993@gmail.com>
2025-03-27 09:49:03 +0200

    I imposed text on image
    I imposed text on image.

--- server/invitationgenerator/routes/invitations.js
+++ server/invitationgenerator/routes/invitations.js
@@ -20,10 +20,16 @@ router.post('/getType', function (req, res) {
 router.post('/getInvitation', async function (req, res) {
     const file = await readFile(`${path}\\public\\images\\backgrounds\\${req['body']['invitation_details']['background_image']}`)
     const img = sharp(file)
-    const textSVG = Buffer.from(`<svg width="400" height="400">
-                        <text x="50%" y="200" font-size="40" text-anchor="middle">
-                            ${req['body']['invitation_details']['greeting']}
-                        </text>
+    const textSVG = Buffer.from(`<svg width="500" height="700">
+                        <text x="50%" y="30" text-anchor="middle"> ${req['body']['invitation_details']['greeting']} ${req['body']['invitation_details']['invitees_names']} </text>
+                        <text x="50%" y="60" text-anchor="middle"> ${req['body']['invitation_details']['message']} </text>
+                        <text x="50%" y="90" text-anchor="middle"> ${req['body']['invitation_details']['who']}, ${req['body']['invitation_details']['inviting_names']}, ${req['body']['invitation_details']['body']} </text>
+                        <text x="50%" y="120" text-anchor="middle"> ${req['body']['invitation_details']['event_title']} </text>
+                        <text x="50%" y="150" text-anchor="middle"> Дата: ${req['body']['invitation_details']['first_date']} Час: ${req['body']['invitation_details']['first_time']} Місце: ${req['body']['invitation_details']['first_place']}  </text>
+                        <text x="50%" y="180" text-anchor="middle"> ${req['body']['invitation_details']['party_title']} </text>
+                        <text x="50%" y="210" text-anchor="middle"> Дата: ${req['body']['invitation_details']['second_date']} Час: ${req['body']['invitation_details']['second_time']} Місце: ${req['body']['invitation_details']['second_place']}  </text>
+                        <text x="50%" y="240" text-anchor="middle"> ${req['body']['invitation_details']['assurance']} </text>
+                        <text x="50%" y="270" text-anchor="middle"> ${req['body']['invitation_details']['farewell']}, ${req['body']['invitation_details']['inviting_names']}. </text>
                     </svg>`)
     const result = await img.composite([{ input: textSVG }]).toBuffer()
     await writeFile(`${path}\\public\\images\\invitations\\res.png`, result)
