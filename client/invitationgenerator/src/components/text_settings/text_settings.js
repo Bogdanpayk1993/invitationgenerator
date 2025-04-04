e62d37537daab43567142add10ec9cc2fe70bf71
Bogdanpayk1993 <body.payk.1993@gmail.com>
2025-04-04 17:38:14 +0300

    I added to invitation third event
    I added to invitation third event.

--- client/invitationgenerator/src/components/text_settings/text_settings.js
+++ client/invitationgenerator/src/components/text_settings/text_settings.js
@@ -20,13 +20,13 @@ async function get_invitation_texts(path_to_server, type, set_invitation_texts) 
                 message: json[el]["message"],
                 who: json[el]["who"],
                 body: json[el]["body"],
-                event_title: json[el]["eventtitle"],
-                party_title: json[el]["partytitle"],
+                event_first_title: json[el]["eventfirsttitle"],
+                event_second_title: json[el]["eventsecondtitle"],
+                event_third_title: json[el]["eventthirdtitle"],
                 assurance: json[el]["assurance"],
                 farewell: json[el]["farewell"]
             })
         ))
-
         set_invitation_texts(json1)
     }
 }
@@ -41,12 +41,17 @@ function Text_Settings(props) {
     const second_place = useRef()
     const second_date = useRef()
     const second_time = useRef()
+    const third_place = useRef()
+    const third_date = useRef()
+    const third_time = useRef()
 
     const type = props.type
     const path_to_server = props.path_to_server
     const background_image = props.background_image
     const [invitation_texts, set_invitation_texts] = useState("")
     const [template_type, set_template_type] = useState("")
+
+    let invitation_index = 0;
 
     if (Object.keys(invitation_texts).length == 0) {
         get_invitation_texts(path_to_server, type, set_invitation_texts)
@@ -58,7 +63,7 @@ function Text_Settings(props) {
 
     async function generating_invitation() {
 
-        let invitation_index = invitation_texts.findIndex(el => el["label"] === template_type)
+        invitation_index = invitation_texts.findIndex(el => el["label"] === template_type)
 
         let invitation_details = {
             ...invitation_texts[invitation_index],
@@ -66,11 +71,14 @@ function Text_Settings(props) {
             inviting_names: inviting_names.current.value,
             invitees_names: invitees_names.current.value,
             first_place: first_place.current.value,
-            first_date: first_date.current.value,
-            first_time: first_time.current.value,
+            first_date: "27.04.2025",
+            first_time: "10:00",
             second_place: second_place.current.value,
-            second_date: second_date.current.value,
-            second_time: second_time.current.value
+            second_date: "27.04.2025",
+            second_time: "14:00",
+            third_place: third_place.current.value,
+            third_date: "27.04.2025",
+            third_time: "18:00"
         }
         
         let json = await Send_Request_For_Database({ link: `${path_to_server}/invitations/getInvitation`, invitation_details: invitation_details })
@@ -119,7 +127,7 @@ function Text_Settings(props) {
                         <hr />
                         <div>
                             <div className="Part_Name">
-                                Офіційна частина
+                                {invitation_texts[invitation_index]["event_first_title"]}
                             </div>
                         </div>
                         <div>
@@ -149,7 +157,7 @@ function Text_Settings(props) {
                         <hr />
                         <div>
                             <div className="Part_Name">
-                                Банкет
+                                {invitation_texts[invitation_index]["event_second_title"]}
                             </div>
                         </div>
                         <div>
@@ -174,6 +182,36 @@ function Text_Settings(props) {
                             </div>
                             <div>
                                 <input type="time" ref={second_time} />
+                            </div>
+                        </div>
+                        <hr />
+                        <div>
+                            <div className="Part_Name">
+                                {invitation_texts[invitation_index]["event_third_title"]}
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                Адреса проведення -
+                            </div>
+                            <div>
+                                <input value={"Ужгород"}  ref={third_place} />
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                Дата -
+                            </div>
+                            <div>
+                                <input type="date" ref={third_date} />
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                Час -
+                            </div>
+                            <div>
+                                <input type="time" ref={third_time} />
                             </div>
                         </div>
                         <hr />
--- server/invitationgenerator/routes/invitations.js
+++ server/invitationgenerator/routes/invitations.js
@@ -10,7 +10,7 @@ var writeFile = fsPromises.writeFile;
 var appRootPath = require('app-root-path');
 var path = appRootPath.path;
 
-var sharp = require('sharp');  
+var sharp = require('sharp');
 
 router.post('/getType', function (req, res) {
     const result = db.prepare(`SELECT * FROM invitations WHERE type='${req['body']['type']}'`).all()
@@ -20,19 +20,29 @@ router.post('/getType', function (req, res) {
 router.post('/getInvitation', async function (req, res) {
     const file = await readFile(`${path}\\public\\images\\backgrounds\\${req['body']['invitation_details']['background_image']}`)
     const img = sharp(file)
-    const textSVG = Buffer.from(`<svg width="600" height="600">
-                        <text x="50%" y="100" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['greeting']} ${req['body']['invitation_details']['invitees_names']} </text>
-                        <text x="50%" y="160" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['message']} </text>
-                        <text x="50%" y="200" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['who']}, ${req['body']['invitation_details']['inviting_names']}, ${req['body']['invitation_details']['body']} </text>
-                        <text x="50%" y="260" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['event_title']} </text>
-                        <text x="50%" y="300" text-anchor="middle" font-size="20"> Дата: ${req['body']['invitation_details']['first_date']} Час: ${req['body']['invitation_details']['first_time']} </text>
-                        <text x="50%" y="340" text-anchor="middle" font-size="20"> Адреса: ${req['body']['invitation_details']['first_place']} </text>
-                        <text x="50%" y="400" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['party_title']} </text>
-                        <text x="50%" y="440" text-anchor="middle" font-size="20"> Дата: ${req['body']['invitation_details']['second_date']} Час: ${req['body']['invitation_details']['second_time']} </text>
-                        <text x="50%" y="480" text-anchor="middle" font-size="20"> Адреса: ${req['body']['invitation_details']['second_place']} </text>
-                        <text x="50%" y="540" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['assurance']} </text>
-                        <text x="50%" y="580" text-anchor="middle" font-size="20"> ${req['body']['invitation_details']['farewell']}, ${req['body']['invitation_details']['inviting_names']}. </text>
-                    </svg>`)
+    const textSVG = Buffer.from(`<svg width="600" height="800">
+                                    <defs>
+                                        <style>
+                                            .text {
+                                                font-size: 20;
+                                            }
+                                        </style>
+                                    </defs>
+                                    <text x="50%" y="100" text-anchor="middle" class="text"> ${req['body']['invitation_details']['greeting']} ${req['body']['invitation_details']['invitees_names']} </text>
+                                    <text x="50%" y="160" text-anchor="middle" class="text"> ${req['body']['invitation_details']['message']} </text>
+                                    <text x="50%" y="200" text-anchor="middle" class="text"> ${req['body']['invitation_details']['who']}, ${req['body']['invitation_details']['inviting_names']}, ${req['body']['invitation_details']['body']} </text>
+                                    <text x="50%" y="260" text-anchor="middle" class="text"> ${req['body']['invitation_details']['event_first_title']} </text>
+                                    <text x="50%" y="300" text-anchor="middle" class="text"> Дата: ${req['body']['invitation_details']['first_date']} Час: ${req['body']['invitation_details']['first_time']} </text>
+                                    <text x="50%" y="340" text-anchor="middle" class="text"> Адреса: ${req['body']['invitation_details']['first_place']} </text>
+                                    <text x="50%" y="400" text-anchor="middle" class="text"> ${req['body']['invitation_details']['event_second_title']} </text>
+                                    <text x="50%" y="440" text-anchor="middle" class="text"> Дата: ${req['body']['invitation_details']['second_date']} Час: ${req['body']['invitation_details']['second_time']} </text>
+                                    <text x="50%" y="480" text-anchor="middle" class="text"> Адреса: ${req['body']['invitation_details']['second_place']} </text>
+                                    <text x="50%" y="540" text-anchor="middle" class="text"> ${req['body']['invitation_details']['event_third_title']} </text>
+                                    <text x="50%" y="580" text-anchor="middle" class="text"> Дата: ${req['body']['invitation_details']['third_date']} Час: ${req['body']['invitation_details']['third_time']} </text>
+                                    <text x="50%" y="620" text-anchor="middle" class="text"> Адреса: ${req['body']['invitation_details']['third_place']} </text>
+                                    <text x="50%" y="680" text-anchor="middle" class="text"> ${req['body']['invitation_details']['assurance']} </text>
+                                    <text x="50%" y="720" text-anchor="middle" class="text"> ${req['body']['invitation_details']['farewell']}, ${req['body']['invitation_details']['inviting_names']}. </text>
+                                 </svg>`)
     const result = await img.composite([{ input: textSVG }]).toBuffer()
     await writeFile(`${path}\\public\\images\\invitations\\res.png`, result)
 })
