b3ec56579f6dd4535d74407264d1cd7712f63512
Bogdanpayk1993 <body.payk.1993@gmail.com>
2025-04-12 11:54:50 +0300

    Functions in component ``edit template`` is ready
    Functions in component ``edit template`` is ready.

--- client/invitationgenerator/src/components/edit_template/edit_template.js
+++ client/invitationgenerator/src/components/edit_template/edit_template.js
@@ -15,49 +15,60 @@ function Edit_Template(props) {
         set_invitation_text(new_invitation_text)
     }
 
-    function cancel_changes() {
-        set_invitation_text([])
+    function add_input_row(i) {
+        let new_invitation_text = invitation_text.slice()
+        new_invitation_text.splice(i + 1, 0, { text: "", offset: 30 })
+        set_invitation_text(new_invitation_text)
+    }
+
+    function delete_input_row(i) {
+        let new_invitation_text = invitation_text.slice()
+        new_invitation_text.splice(i, 1)
+        set_invitation_text(new_invitation_text)
         set_number_input(-1)
+    }
+
+    function change_offset(i) {
+        let offset
+        let new_invitation_text = invitation_text.slice()
+        if (invitation_text[i]['offset'] == 30) {
+            offset = 60
+        } else {
+            offset = 30
+        }
+        new_invitation_text[i]['offset'] = offset
+        set_invitation_text(new_invitation_text)
     }
 
     return (
         <div className="edit_template">
-            <div className="edit_template_menu">
+            <div className="edit_template_body">
                 <div>
-                    <button onClick={() => cancel_changes()}> Відхилити зміни </button>
-                </div>
-                <div>
-                    <button onClick={() => set_edit_template_regime(false)}> Зберігти зміни </button>
-                </div>
-            </div>
-            <div className="edit_template_container">
-                {
-                    invitation_text.map((el, i) => (
-                        <div className={invitation_text[i]['offset'] == 60 ? "with" : "without"} >
-                            <div className="input_row">
-                                <input onClick={() => set_number_input(i)} onChange={(event) => change_invitation_text(event, i)} value={invitation_text[i]['text']} key={i} />
-                            </div>
-                            {
-                                number_input == i ?
-                                    <div>
-                                        <div className="menu">
-                                            <input type="button" value="Додати" />
-                                            <input type="button" value="Видалити" />
-                                            <input type="checkbox" checked={invitation_text[i]['offset'] == 60 ? true : false} /> <label> Відступ </label>
+                    {
+                        invitation_text.map((el, i) => (
+                            <div className={invitation_text[i]['offset'] == 60 ? "with" : "without"} >
+                                <div className="input_row">
+                                    <input onClick={() => set_number_input(i)} onChange={(event) => change_invitation_text(event, i)} placeholder="____________________________" value={invitation_text[i]['text']} key={i} />
+                                </div>
+                                {
+                                    number_input == i ?
+                                        <div>
+                                            <div className="menu">
+                                                <input type="button" onClick={() => add_input_row(i)} value="Додати" />
+                                                <input type="button" onClick={() => delete_input_row(i)} value="Видалити" />
+                                                <label>
+                                                    <input type="checkbox" onChange={() => change_offset(i)} checked={invitation_text[i]['offset'] == 60 ? true : false} />
+                                                    Відступ
+                                                </label>
+                                                <input type="button" onClick={() => set_number_input(-1)} value="X" />
+                                            </div>
                                         </div>
-                                    </div>
-                                    : null
-                            }
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="closed_menu">
-                {
-                    number_input != -1 ?
-                        <button onClick={() => set_number_input(-1)} > Приховати меню </button>
-                        : null
-                }
+                                        : null
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
